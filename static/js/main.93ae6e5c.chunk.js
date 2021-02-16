f8cd7719ed9bb2c34438b85037e213571e2f8cf6
Paulikas <Deivydas.Paulikas@dematic.com>
2021-02-16 11:39:10 +0100

    Updates

--- static/js/main.93ae6e5c.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpaboutme=this.webpackJsonpaboutme||[]).push([[0],{10:function(e,t,n){"use strict";n.r(t);var c=n(0),r=n(1),s=n.n(r),i=n(3),a=n.n(i);n(9);var o=function(){return Object(c.jsx)("div",{className:"main",children:Object(c.jsx)("h1",{children:"SUDAS"})})};a.a.render(Object(c.jsx)(s.a.StrictMode,{children:Object(c.jsx)(o,{})}),document.getElementById("root"))},9:function(e,t,n){}},[[10,1,2]]]);
-//# sourceMappingURL=main.93ae6e5c.chunk.js.map
